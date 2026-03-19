e7ab15a361a1e916aec1f8a4b058fe42f4ee4f44
JJoaoll <joaoduos@gmail.com>
2026-03-19 16:49:01 -0300

    sync

---
+++ src/GaussJacobi/Serial.hs
@@ -0,0 +1,19 @@
+
+module GaussJacobi.Serial where
+
+import Prelude hiding ((<>))
+import Numeric.LinearAlgebra
+
+data LinearSystem = LS 
+
+initialSolution :: Matrix Double -> Vector Double
+initialSolution m = undefined
+
+v1 :: Matrix Double
+v1 =  (4><4) [1..]
+
+v2 :: Matrix Double
+v2 =  (4><4) [10..]
+
+
+
